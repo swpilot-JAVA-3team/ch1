4270aa53784555ea99da4f37a38036e93d7e2a76
권혁주 <toturak1@gmail.com>
2025-05-07 20:54:19 +0900

    hello

--- src/day_0507/HelloBiodome01.java
+++ src/day_0507/HelloBiodome01.java
@@ -15,13 +15,15 @@ public class HelloBiodome01 {
 		
 		System.out.println("어서오세요 "+ name +" 님!");
 		
+		/* System.in
+		 * 입력받기
 		System.out.print("이름을 입력하십시오. :");
 		
 		byte buffer[] = new byte[1024];
 		int byteRead = System.in.read(buffer);
 		String name2 = new String(buffer,0,byteRead);
 		System.out.println("어서오십시오. "+name+" 님.");
-
+		 */
 		
 		
 	}
