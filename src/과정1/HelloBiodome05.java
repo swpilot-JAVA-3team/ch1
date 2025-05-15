dc88fe9b6853e7848e5a4bdbdbca37d997c2770b
권혁주 <toturak1@gmail.com>
2025-05-15 21:28:51 +0900

    feat: complete

--- src/과정1/HelloBiodome05.java
+++ src/과정1/HelloBiodome05.java
@@ -1,10 +1,27 @@
 package 과정1;
 
 public class HelloBiodome05 {
+	
+	public static int calcul()
+	{
+		int g,h;
+				
+		for (g=0;g<16;g++)
+		{
+			for(h=0;h<16;h++)
+			{
+				if((g&1>>g<<2|h+g^h)==1&&(g%2<<h>>g|1&0^0)==2)
+				{
+					return ((h*h+g)*(h<<h)+(g<<g));
+				}
+			}
+		}
+		return 0;
+	}
 	public static void main(String[] args)
 	{
-		byte g, h;
-		// g & 1 >> g << 2 | b + g^b = 1
+		int result = calcul();
+		System.out.println(result);
 		
 	}
 }
