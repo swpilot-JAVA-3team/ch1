f8c55ebe5e21339c44d2795f8965915cf7b74e1b
colabdaegu <colab.daegu@gmail.com>
2025-05-17 15:29:13 +0900

    김현수_

--- src/javacc/HelloBiodome08.java
+++ src/javacc/HelloBiodome08.java
@@ -36,47 +36,46 @@ public class HelloBiodome08 {
 		
 		StringBuilder stringBuild = new StringBuilder();
 		
-		int i = 0;
+		int i = 0;  // 입력받은 문장을 검토할 때 사용될 인덱스
 		
 		
 		while (i < input.length()) {
 			boolean found = false;
-			for (String choose_dict : dict) {
+			for (String choose_dict : dict) {  // 사전에서 단어를 하나씩 가져와서 choose_dict에 넣음
 				int dict_length = choose_dict.length(); 
-				//System.out.println(dict_length);
-				int correct_count = 1;
+
+				int correct_count = 1;  // 입력문장과 사전단어의 위치를 비교할 인덱스
 				
-				if (input.charAt(i) == choose_dict.charAt(0)) {
+				if (input.charAt(i) == choose_dict.charAt(0)) {   // 입력문장에서 비교할 첫 알파벳과 사전단어의 첫 알파벳을 비교
 					
-					
-					for (int dict_index = 1; dict_index < dict_length; dict_index++) {
-						if (input.charAt(i + dict_index) == choose_dict.charAt(dict_index)) {
+					for (int dict_index = 1; dict_index < dict_length; dict_index++) {    // (사전단어의 첫 알파벳은 검사했으므로) 사전단어의 두 번째 알파벳부터 마지막 알파벳까지 반복
+						if (input.charAt(i + dict_index) == choose_dict.charAt(dict_index)) {  // 알파벳 자리수마다 서로 동일한 알파벳인지 검사
 							
 							correct_count++;
 							
 						} else {
-							break;
+							break;  // 알파벳이 서로 다르면 중단
 						}
 					}
-					if (dict_length == correct_count) {
+					if (dict_length == correct_count) {   // 입력문장과 사전단어의 알파벳이 처음부터 끝까지 모두 동일하면
 						stringBuild.append(choose_dict);
-						i += dict_length;
+						i += dict_length;   // 사전단어 글자수만큼 입력문장에서 검사할 인덱스를 증감
 						found = true;
-						if (i != input.length() - 1) {
-							stringBuild.append(" ");
+						if (i != input.length() - 1) {  // 입력문장의 마지막 단어가 아니면
+							stringBuild.append(" ");  // append한 단어 끝에 공백 추가
 						}
 						break;
 					}
 				}
 			}
-			if (!found) {
+			if (!found) {  // 사전에서 하나도 찾지 못했다면
 				stringBuild.append(input.charAt(i));
-				i++;
+				i++;  // 입력문장에서 검사할 인덱스를 한 자리만 증감
 			}
 		}
 		
-		if (stringBuild.charAt(stringBuild.length() - 1) == ' ') stringBuild.deleteCharAt(stringBuild.length() - 1);
-		if (!input.toString().endsWith("?")) stringBuild.append(".");
+		if (stringBuild.charAt(stringBuild.length() - 1) == ' ') stringBuild.deleteCharAt(stringBuild.length() - 1);  // 문장의 마지막에 공백이 있다면 제거
+		if (!input.toString().endsWith("?")) stringBuild.append(".");  // 입력 문장이 ?로 안 끝났었다면 마지막에 .를 추가
         
 		
 		System.out.println(stringBuild);
